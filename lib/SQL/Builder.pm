1529dba295780df8a1618b3b36989cc8d28993ec
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-11-27 11:37:19 +0900

    initial import

---
+++ lib/SQL/Builder.pm
@@ -0,0 +1,39 @@
+package SQL::Builder;
+use strict;
+use warnings;
+use 5.008001;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+SQL::Builder -
+
+=head1 SYNOPSIS
+
+  use SQL::Builder;
+
+=head1 DESCRIPTION
+
+SQL::Builder is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
