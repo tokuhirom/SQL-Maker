68daa52ac311d59284d97205529d7b7793d43c03
Tokuhiro Matsuno <tokuhirom@gmail.com>
2010-12-04 17:53:55 +0900

    refactor

---
+++ lib/SQL/Builder/Condition.pm
@@ -0,0 +1,56 @@
+package SQL::Builder::Condition;
+use strict;
+use warnings;
+use utf8;
+use SQL::Builder::Part;
+use overload
+    '&' => sub { $_[0]->compose_and($_[1]) },
+    '|' => sub { $_[0]->compose_or($_[1]) },
+    fallback => 1;
+
+sub new {
+    my $class = shift;
+    my %args = @_==1 ? %{$_[0]} : @_;
+    bless {sql => [], bind => [], %args}, $class;
+}
+
+sub add {
+    my ( $self, $col, $val ) = @_;
+
+    my ( $term, $bind ) = SQL::Builder::Part->make_term( $col, $val );
+    push @{ $self->{sql} }, "($term)";
+    push @{ $self->{bind} },  @$bind;
+
+    return $self; # for influent interface
+}
+
+sub compose_and {
+    my ($self, $other) = @_;
+
+    return SQL::Builder::Where->new(
+        sql => ['(' . $self->as_sql() . ') AND (' . $other->as_sql() . ')'],
+        bind => [@{$self->{bind}}, @{$other->{bind}}],
+    );
+}
+
+sub compose_or {
+    my ($self, $other) = @_;
+
+    return SQL::Builder::Where->new(
+        sql => ['(' . $self->as_sql() . ') OR (' . $other->as_sql() . ')'],
+        bind => [@{$self->{bind}}, @{$other->{bind}}],
+    );
+}
+
+sub as_sql {
+    my ($self) = @_;
+    return join(' AND ', @{$self->{sql}});
+}
+
+sub bind {
+    my $self = shift;
+    return @{$self->{bind}};
+}
+
+1;
+
--- lib/SQL/Builder/Where.pm
+++ lib/SQL/Builder/Where.pm
@@ -2,45 +2,7 @@ package SQL::Builder::Where;
 use strict;
 use warnings;
 use utf8;
-use SQL::Builder::Part;
-use overload
-    '&' => sub { $_[0]->compose_and($_[1]) },
-    '|' => sub { $_[0]->compose_or($_[1]) },
-    fallback => 1;
-
-sub new {
-    my $class = shift;
-    my %args = @_==1 ? %{$_[0]} : @_;
-    bless {sql => [], bind => [], %args}, $class;
-}
-
-sub add {
-    my ( $self, $col, $val ) = @_;
-
-    my ( $term, $bind ) = SQL::Builder::Part->make_term( $col, $val );
-    push @{ $self->{sql} }, "($term)";
-    push @{ $self->{bind} },  @$bind;
-
-    return $self; # for influent interface
-}
-
-sub compose_and {
-    my ($self, $other) = @_;
-
-    return SQL::Builder::Where->new(
-        sql => ['(' . $self->as_sql() . ') AND (' . $other->as_sql() . ')'],
-        bind => [@{$self->{bind}}, @{$other->{bind}}],
-    );
-}
-
-sub compose_or {
-    my ($self, $other) = @_;
-
-    return SQL::Builder::Where->new(
-        sql => ['(' . $self->as_sql() . ') OR (' . $other->as_sql() . ')'],
-        bind => [@{$self->{bind}}, @{$other->{bind}}],
-    );
-}
+use parent qw/SQL::Builder::Condition/;
 
 sub as_sql {
     my ($self, $need_prefix) = @_;
@@ -49,10 +11,4 @@ sub as_sql {
     return $sql;
 }
 
-sub bind {
-    my $self = shift;
-    return @{$self->{bind}};
-}
-
 1;
-
