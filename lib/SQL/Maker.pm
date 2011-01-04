10364eb2807fbc5b61cb0c16ebad38c87174ebb1
tokuhirom <tokuhirom@gmail.com>
2011-01-04 15:17:41 +0900

    Checking in changes prior to tagging of version 0.04.
    Changelog diff is:
    
    diff --git a/Changes b/Changes
    index baaf4d5..3d43549 100644
    --- a/Changes
    +++ b/Changes
    @@ -1,5 +1,9 @@
     Revision history for Perl extension SQL::Maker
    
    +0.04
    +
    +    - add space each comma on using IN statement(zigorou)
    +
     0.03
    
         - 60ded52 (HEAD, zigorou/master, origin/master, master) remove last new_line on as_sql(), add tests for different new args(zigorou++)

--- lib/SQL/Maker.pm
+++ lib/SQL/Maker.pm
@@ -2,7 +2,7 @@ package SQL::Maker;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '0.03';
+our $VERSION = '0.04';
 use Class::Accessor::Lite 0.05 (
     ro => [qw/quote_char name_sep new_line driver select_class/],
 );
