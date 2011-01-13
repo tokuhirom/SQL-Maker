57cd78ce8040ce7c9bf022eb93cb653bed94e11e
makamaka <makamaka.donzoko@gmail.com>
2011-01-13 23:27:15 +0900

    changed methods to functions and remove overload

--- lib/SQL/Maker/SelectSet.pm
+++ lib/SQL/Maker/SelectSet.pm
@@ -1,17 +1,52 @@
 package SQL::Maker::SelectSet;
 use strict;
 use warnings;
-use parent qw(SQL::Maker::Select);
+use parent qw(Exporter SQL::Maker::Select);
+use Scalar::Util ();
 
+our @EXPORT_OK = qw(union union_all intersect intersect_all except except_all);
+
+# Class methods
+
+sub union ($$) {
+    SQL::Maker::SelectSet->new_set( 'UNION', @_ );
+}
+
+sub union_all ($$) {
+    SQL::Maker::SelectSet->new_set( 'UNION ALL', @_ );
+}
+
+sub intersect ($$) {
+    SQL::Maker::SelectSet->new_set( 'INTERSECT', @_ );
+}
+
+sub intersect_all ($$) {
+    SQL::Maker::SelectSet->new_set( 'INTERSECT ALL', @_ );
+}
+
+sub except ($$) {
+    SQL::Maker::SelectSet->new_set( 'EXCEPT', @_ );
+}
+
+sub except_all ($$) {
+    SQL::Maker::SelectSet->new_set( 'EXCEPT ALL', @_ );
+}
+
+sub _compose_set {
+    return SQL::Maker::SelectSet->new_set( @_ );
+}
 
 sub new_set {
     my ( $class, $operator, $s1, $s2 ) = @_;
-    my $set = $class->new( new_line => $s1->new_line );
 
-    if ( ref $s2 eq 'ARRAY' ) { # by SQL::Maker::Select::all
-        $s2 = $s2->[1];
-        $operator .= ' ALL';
+    for my $s ( $s1, $s2 ) {
+        unless ( Scalar::Util::blessed( $s ) and $s->isa('SQL::Maker::Select') ) {
+            require Carp;
+            Carp::croak( "$s is not an object inherited from SQL::Maker::Select." );
+        }
     }
+
+    my $set = $class->new( new_line => $s1->new_line );
 
     $set->_expand_statement( $s1 );
     $set->add_set_operator( $operator );
@@ -19,6 +54,10 @@ sub new_set {
 
     return $set;
 }
+
+#
+# Methods
+#
 
 sub _expand_statement {
     my ( $self, $s ) = @_;
@@ -75,7 +114,7 @@ sub as_sql {
 sub bind {
     my ( $self ) = @_;
     my @binds;
-    for my $select ( @{ $self->{ selects } } ) {
+    for my $select ( @{ $self->{ statements } } ) {
         push @binds, $select->bind;
     }
     return @binds;
@@ -87,40 +126,69 @@ __END__
 
 =head1 NAME
 
-SQL::Maker::SelectSet - SQL::Maker::Select set
+SQL::Maker::SelectSet - provides set functions
 
 =head1 SYNOPSIS
 
+    use SQL::Maker::SelectSet qw(union_all except);
     my $s1 = SQL::Maker::Select ->new()
                                 ->add_select('foo')
                                 ->add_from('t1');
     my $s2 = SQL::Maker::Select ->new()
                                 ->add_select('bar')
                                 ->add_from('t2');
-
-    SQL::Maker::SelectSet->new_set( 'UNION', $s1, $s2 )->as_sql;
-    # => "SELECT foo FROM t1 UNION SELECT bar FROM t2"
-    # => $s1->union( $s2 )->as_sql;
-    # => do{ $s1 + $s2 }->as_sql;
-
-    SQL::Maker::SelectSet->new_set( 'EXCEPT', $s1, $s2->all )->as_sql;
-    # => "SELECT foo FROM t1 EXCEPT ALL SELECT bar FROM t2"
-    # => $s1->except( all $s2 )->as_sql;
-    # => do{ $s1 - all $s2 }->as_sql;
+    union_all( $s1, $s2 )->as_sql;
+    # => SQL::Maker::SelectSet->new_set( 'UNION ALL', $s1, $s2 )->as_sql;
+    # => "SELECT foo FROM t1 UNION ALL SELECT bar FROM t2"
+    except( $s1, $s2 )->as_sql;
+    # => SQL::Maker::SelectSet->new_set( 'EXCEPT', $s1, $s2->all )->as_sql;
+    # => "SELECT foo FROM t1 EXCEPT SELECT bar FROM t2"
 
 =head1 DESCRIPTION
 
-Set representation inherited from L<SQL::Maker::Select>.
+This module provides some set functions which return a SQL::Maker::SelectSet object
+inherited from L<SQL::Maker::Select>.
 
-=head1 METHODS
+SQL::Maker::SelectSet can call SQL::Maker::Select methods
+except of C<add_select>, C<add_from> and C<add_join>.
 
-Can call SQL::Maker::Select methods except of C<add_select>, C<add_from> and C<add_join>.
+=head1 FUNCTION
 
-=over4
+=over 4
+
+=item union($select :SQL::Maker::Select | $set :SQL::Maker::SelectSet) : SQL::Maker::SelectSet
+
+Tow statements are combined by C<UNION>.
+
+=item union_all($select :SQL::Maker::Select | $set :SQL::Maker::SelectSet) : SQL::Maker::SelectSet
+
+Tow statements are combined by C<UNION ALL>.
+
+=item intersect($select :SQL::Maker::Select | $set :SQL::Maker::SelectSet) : SQL::Maker::SelectSet
+
+Tow statements are combined by C<INTERSECT>.
+
+=item intersect_all($select :SQL::Maker::Select | $set :SQL::Maker::SelectSet) : SQL::Maker::SelectSet
+
+Tow statements are combined by C<INTERSECT ALL>.
+
+=item except($select :SQL::Maker::Select | $set :SQL::Maker::SelectSet) : SQL::Maker::SelectSet
+
+Tow statements are combined by C<EXCEPT>.
+
+=item except($select :SQL::Maker::Select | $set :SQL::Maker::SelectSet) : SQL::Maker::SelectSet
+
+Tow statements are combined by C<EXCEPT ALL>.
+
+=back
+
+=head1 Class Method
+
+=over 4
 
 =item SQL::Maker::SelectSet->new_set( $operator, $one, $another)
 
-$opretaor is a set opretaor.
+$opretaor is a set operator (ex. C<UNION>).
 $one and $another are SQL::Maker::Select object or SQL::Maker::SelectSet object.
 It returns a SQL::Maker::SelectSet object.
 
