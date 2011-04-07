83ad30c1f5b212672d3d5fc4e771ba5a2888da6d
yamaguchi.toru <zigorou@cpan.org>
2011-04-07 22:20:17 +0900

    considering quote and supports 'INSERT IGNORE' on insert_multi, add update_multi() method

--- lib/SQL/Maker/Plugin/InsertMulti.pm
+++ lib/SQL/Maker/Plugin/InsertMulti.pm
@@ -3,45 +3,100 @@ use strict;
 use warnings;
 use utf8;
 
-our @EXPORT = qw/insert_multi/;
+our @EXPORT = qw/insert_multi update_multi/;
 
 # for mysql
 sub insert_multi {
-    my ($self, $table, $args, $binds) = @_;
-    return unless @$args;
+    # my ($self, $table, $cols, $binds, $opts) = @_;
+    # my ($self, $table, $colvals, $opts) = @_;
+    my ( $self, $table, @args ) = @_;
+    return unless @{$args[0]};
 
-    # setting cols
-    my @cols;
-    my $first_arg = $args->[0];
-    my $is_cols   = ( !ref $first_arg ) ? 1 : 0;
+    my (@cols, @bind, $opts);
+    my $first_arg = $args[0]->[0];
+    my $is_colvals = ( ref $first_arg ) ? 1 : 0;
 
-    if ( $is_cols ) {
-	@cols = @$args;
+    if ( $is_colvals ) {
+	@cols = keys %{$first_arg};
+	@bind = map { @$_{@cols} } @{$args[0]};
+	$opts = $args[1] || +{};
     }
     else {
-	for my $col (keys %{$first_arg}) {
-	    push @cols, $col;
-	}
+	@cols = @{$args[0]};
+	@bind = map { @$_ } @{$args[1]};
+	$opts = $args[2] || +{};
     }
 
-    my @bind;
-    if ( $is_cols ) {
-	@bind = map { @$_ } @$binds;
-    }
-    else {
-	for my $arg (@{$args}) {
-	    for my $col (@cols) {
-		push @bind, $arg->{$col};
-	    }
-	}
-    }
-
-    my $sql = "INSERT INTO $table" . $self->new_line;
-       $sql .= '(' . join(', ', @cols) . ')' . $self->new_line . "VALUES ";
+    my $prefix = $opts->{prefix} || 'INSERT INTO';
+    my $quoted_table = $self->_quote($table);
+    my @quoted_cols  = map { $self->_quote($_) } @cols;
+    
+    my $sql = "$prefix $quoted_table" . $self->new_line;
+       $sql .= '(' . join(', ', @quoted_cols) . ')' . $self->new_line . "VALUES ";
 
     my $values = '(' . join(', ', ('?') x @cols) . ')' . $self->new_line;
     $sql .= join(',', ($values) x (scalar(@bind) / scalar(@cols)));
     $sql =~ s/$self->{new_line}+$//;
+
+    return ($sql, @bind);
+}
+
+sub update_multi {
+    ### my ( $self, $table, $cols, $values, $sets, $opts ) = @_;
+    ### my ( $self, $table, $colvals, $sets, $opts ) = @_;
+    my ( $self, $table, @args ) = @_;
+    my $is_cols = !ref $args[0]->[1] ? 1 : 0;
+
+    return unless @{$args[0]};
+
+    my (@cols, @bind, $sets, $opts);
+    my $first_arg = $args[0]->[0];
+    my $is_colvals = ( ref $first_arg ) ? 1 : 0;
+
+    if ( $is_colvals ) {
+	@cols = keys %{$first_arg};
+	@bind = map { @$_{@cols} } @{$args[0]};
+	$sets = $args[1];
+	$opts = $args[2] || +{};
+    }
+    else {
+	@cols = @{$args[0]};
+	@bind = map { @$_ } @{$args[1]};
+	$sets = $args[2];
+	$opts = $args[3] || +{};
+    }
+
+    my $prefix = $opts->{prefix} || 'INSERT INTO';
+    my $quoted_table = $self->_quote($table);
+    my @quoted_cols  = map { $self->_quote($_) } @cols;
+
+    my $sql = "$prefix $quoted_table" . $self->new_line;
+       $sql .= '(' . join(', ', @quoted_cols) . ')' . $self->new_line . "VALUES ";
+
+    my $values = '(' . join(', ', ('?') x @cols) . ')' . $self->new_line;
+    $sql .= join(',', ($values) x (scalar(@bind) / scalar(@cols)));
+
+    my @sets = ref $sets eq 'HASH' ? %$sets : @$sets;
+    my @update_sets;
+    while (my ($col, $val) = splice @sets, 0, 2) {
+	my $quoted_col = $self->_quote($col);
+        if ( ref $val eq 'SCALAR' ) {
+            # $builder->update(foo => { created_on => \"NOW()" });
+            push @update_sets, "$quoted_col = " . $$val;
+        }
+	elsif ( ref $val eq 'REF' && ref $$val eq 'ARRAY' ) {
+	    my ( $stmt, @sub_bind ) = @{$$val};
+	    push @update_sets, "$quoted_col = " . $stmt;
+	    push @bind, @sub_bind;
+	}
+	else {
+            # normal values
+            push @update_sets, "$quoted_col = ?";
+            push @bind, $val;
+        }
+    }
+
+    $sql .= "ON DUPLICATE KEY UPDATE " . join(', ', @update_sets);
 
     return ($sql, @bind);
 }
--- t/plugins/01_insert_multi.t
+++ t/plugins/01_insert_multi.t
@@ -11,7 +11,7 @@ sub ordered_hashref {
 
 SQL::Maker->load_plugin('InsertMulti');
 
-subtest 'insert_multi( $table, \@colvals )' => sub {
+subtest 'insert_multi( $table, \@colvals, \%opts )' => sub {
     subtest 'ok' => sub {
 	my $builder = SQL::Maker->new(driver => 'mysql');
 	my ( $sql, @binds ) = $builder->insert_multi(
@@ -20,7 +20,7 @@ subtest 'insert_multi( $table, \@colvals )' => sub {
 		ordered_hashref( bar => 'bee', john => 'row' )
 	    ]
 	);
-	is $sql, "INSERT INTO foo\n(bar, john)\nVALUES (?, ?)\n,(?, ?)";
+	is $sql, "INSERT INTO `foo`\n(`bar`, `john`)\nVALUES (?, ?)\n,(?, ?)";
 	is join(',', @binds), 'baz,man,bee,row';
     };
 
@@ -32,18 +32,41 @@ subtest 'insert_multi( $table, \@colvals )' => sub {
 		ordered_hashref( john => 'row', bar => 'bee' )
 	    ]
 	);
-	is $sql, "INSERT INTO foo\n(bar, john)\nVALUES (?, ?)\n,(?, ?)";
+	is $sql, "INSERT INTO `foo`\n(`bar`, `john`)\nVALUES (?, ?)\n,(?, ?)";
+	is join(',', @binds), 'baz,man,bee,row';
+    };
+
+    subtest 'insert ignore' => sub {
+	my $builder = SQL::Maker->new(driver => 'mysql');
+	my ( $sql, @binds ) = $builder->insert_multi(
+	    'foo' => [
+		ordered_hashref( bar => 'baz', john => 'man' ),
+		ordered_hashref( bar => 'bee', john => 'row' )
+	    ],
+	    +{ prefix => 'INSERT IGNORE' },
+	);
+	is $sql, "INSERT IGNORE `foo`\n(`bar`, `john`)\nVALUES (?, ?)\n,(?, ?)";
 	is join(',', @binds), 'baz,man,bee,row';
     };
 };
 
-subtest 'insert_multi( $table, \@cols, \@values )' => sub {
+subtest 'insert_multi( $table, \@cols, \@values, \%opts )' => sub {
     subtest 'ok' => sub {
 	my $builder = SQL::Maker->new(driver => 'mysql');
 	my ( $sql, @binds ) = $builder->insert_multi(
 	    'foo' => [ qw/bar john/ ], [ [ qw/baz man/ ], [ qw/bee row/ ], ],
 	);
-	is $sql, "INSERT INTO foo\n(bar, john)\nVALUES (?, ?)\n,(?, ?)";
+	is $sql, "INSERT INTO `foo`\n(`bar`, `john`)\nVALUES (?, ?)\n,(?, ?)";
+	is join(',', @binds), 'baz,man,bee,row';
+    };
+
+    subtest 'insert ignore' => sub {
+	my $builder = SQL::Maker->new(driver => 'mysql');
+	my ( $sql, @binds ) = $builder->insert_multi(
+	    'foo' => [ qw/bar john/ ], [ [ qw/baz man/ ], [ qw/bee row/ ], ],
+	    +{ prefix => 'INSERT IGNORE' },
+	);
+	is $sql, "INSERT IGNORE `foo`\n(`bar`, `john`)\nVALUES (?, ?)\n,(?, ?)";
 	is join(',', @binds), 'baz,man,bee,row';
     };
 };
