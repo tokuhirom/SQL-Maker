9fb9f746c746707866faad720c23bcae3cc0596a
Toru Yamaguchi <zigorou@cpan.org>
2011-04-12 01:45:46 +0900

    remove update_multi() method. change test files layout. supports literal and sub query in values. supports update option to use "ON DUPLICATE KEY UPDATE" on mysql driver.

--- lib/SQL/Maker/Plugin/InsertMulti.pm
+++ lib/SQL/Maker/Plugin/InsertMulti.pm
@@ -3,7 +3,7 @@ use strict;
 use warnings;
 use utf8;
 
-our @EXPORT = qw/insert_multi update_multi/;
+our @EXPORT = qw/insert_multi/;
 
 # for mysql
 sub insert_multi {
@@ -12,91 +12,73 @@ sub insert_multi {
     my ( $self, $table, @args ) = @_;
     return unless @{$args[0]};
 
-    my (@cols, @bind, $opts);
+    my (@cols, @bind, @values, $opts);
     my $first_arg = $args[0]->[0];
     my $is_colvals = ( ref $first_arg ) ? 1 : 0;
 
     if ( $is_colvals ) {
-	@cols = keys %{$first_arg};
-	@bind = map { @$_{@cols} } @{$args[0]};
-	$opts = $args[1] || +{};
+        @cols   = keys %{$first_arg};
+        @values = map { [ @$_{@cols} ] } @{$args[0]};
+        $opts   = $args[1] || +{};
     }
     else {
-	@cols = @{$args[0]};
-	@bind = map { @$_ } @{$args[1]};
-	$opts = $args[2] || +{};
+        @cols   = @{$args[0]};
+        @values = @{$args[1]};
+        $opts   = $args[2] || +{};
     }
 
     my $prefix = $opts->{prefix} || 'INSERT INTO';
     my $quoted_table = $self->_quote($table);
     my @quoted_cols  = map { $self->_quote($_) } @cols;
-    
+
     my $sql = "$prefix $quoted_table" . $self->new_line;
        $sql .= '(' . join(', ', @quoted_cols) . ')' . $self->new_line . "VALUES ";
 
-    my $values = '(' . join(', ', ('?') x @cols) . ')' . $self->new_line;
-    $sql .= join(',', ($values) x (scalar(@bind) / scalar(@cols)));
+    for my $value ( @values ) {
+        my @value_stmt;
+        for my $val (@$value) {
+            if (ref $val eq 'SCALAR') {
+                push @value_stmt, $$val;
+            }
+            elsif (ref $val eq 'REF' && ref $$val eq 'ARRAY') {
+                my ( $stmt, @sub_bind ) = @{$$val};
+                push @value_stmt, $stmt;
+                push @bind, @sub_bind;
+            }
+            else {
+                push @value_stmt, '?';
+                push @bind, $val;
+            }
+        }
+        $sql .= '(' . join(', ', @value_stmt) . '),' . $self->new_line;
+    }
+
+    $sql =~ s/,$self->{new_line}$/$self->{new_line}/;
+
+    if ( $self->{driver} eq 'mysql' && exists $opts->{update} ) {
+        my @sets = ref $opts->{update} eq 'HASH' ? %{$opts->{update}} : @{$opts->{update}};
+        my @update_sets;
+        while (my ($col, $val) = splice @sets, 0, 2) {
+            my $quoted_col = $self->_quote($col);
+            if ( ref $val eq 'SCALAR' ) {
+                # $builder->update(foo => { created_on => \"NOW()" });
+                push @update_sets, "$quoted_col = " . $$val;
+            }
+            elsif ( ref $val eq 'REF' && ref $$val eq 'ARRAY' ) {
+                my ( $stmt, @sub_bind ) = @{$$val};
+                push @update_sets, "$quoted_col = " . $stmt;
+                push @bind, @sub_bind;
+            }
+            else {
+                # normal values
+                push @update_sets, "$quoted_col = ?";
+                push @bind, $val;
+            }
+        }
+        $sql .= "ON DUPLICATE KEY UPDATE " . join(', ', @update_sets) . $self->{new_line};
+    }
+
     $sql =~ s/$self->{new_line}+$//;
-
-    return ($sql, @bind);
-}
-
-sub update_multi {
-    ### my ( $self, $table, $cols, $values, $sets, $opts ) = @_;
-    ### my ( $self, $table, $colvals, $sets, $opts ) = @_;
-    my ( $self, $table, @args ) = @_;
-    my $is_cols = !ref $args[0]->[1] ? 1 : 0;
-
-    return unless @{$args[0]};
-
-    my (@cols, @bind, $sets, $opts);
-    my $first_arg = $args[0]->[0];
-    my $is_colvals = ( ref $first_arg ) ? 1 : 0;
-
-    if ( $is_colvals ) {
-	@cols = keys %{$first_arg};
-	@bind = map { @$_{@cols} } @{$args[0]};
-	$sets = $args[1];
-	$opts = $args[2] || +{};
-    }
-    else {
-	@cols = @{$args[0]};
-	@bind = map { @$_ } @{$args[1]};
-	$sets = $args[2];
-	$opts = $args[3] || +{};
-    }
-
-    my $prefix = $opts->{prefix} || 'INSERT INTO';
-    my $quoted_table = $self->_quote($table);
-    my @quoted_cols  = map { $self->_quote($_) } @cols;
-
-    my $sql = "$prefix $quoted_table" . $self->new_line;
-       $sql .= '(' . join(', ', @quoted_cols) . ')' . $self->new_line . "VALUES ";
-
-    my $values = '(' . join(', ', ('?') x @cols) . ')' . $self->new_line;
-    $sql .= join(',', ($values) x (scalar(@bind) / scalar(@cols)));
-
-    my @sets = ref $sets eq 'HASH' ? %$sets : @$sets;
-    my @update_sets;
-    while (my ($col, $val) = splice @sets, 0, 2) {
-	my $quoted_col = $self->_quote($col);
-        if ( ref $val eq 'SCALAR' ) {
-            # $builder->update(foo => { created_on => \"NOW()" });
-            push @update_sets, "$quoted_col = " . $$val;
-        }
-	elsif ( ref $val eq 'REF' && ref $$val eq 'ARRAY' ) {
-	    my ( $stmt, @sub_bind ) = @{$$val};
-	    push @update_sets, "$quoted_col = " . $stmt;
-	    push @bind, @sub_bind;
-	}
-	else {
-            # normal values
-            push @update_sets, "$quoted_col = ?";
-            push @bind, $val;
-        }
-    }
-
-    $sql .= "ON DUPLICATE KEY UPDATE " . join(', ', @update_sets);
 
     return ($sql, @bind);
 }
@@ -119,7 +101,7 @@ SQL::Maker::Plugin::InsertMulti - insert multiple rows at once on MySQL
 
     my $table = 'foo';
     my @rows = ( +{ bar => 'baz', john => 'man' }, +{ bar => 'bee', john => 'row' } );
-    my $builder = SQL::Maker->new();
+    my $builder = SQL::Maker->new( driver => 'mysql' );
     my ($sql, @binds);
     ### INSERT INTO `foo` (`bar`, `john`) VALUES (?, ?), (?, ?)
     ( $sql, @binds ) = $builder->insert_multi($table, \@rows);
@@ -127,8 +109,8 @@ SQL::Maker::Plugin::InsertMulti - insert multiple rows at once on MySQL
     ### INSERT IGNORE `foo` (`bar`, `john`) VALUES (?, ?), (?, ?)
     ( $sql, @binds ) = $builder->insert_multi($table, [qw/bar john/], [ map { @$_{qw/bar john/} } @rows ], +{ prefix => 'INSERT IGNORE' });
     ### INSERT INTO `foo` (`bar`. `john`) VALUES (?, ?), (?, ?) ON DUPLICATE KEY UPDATE bar => ?
-    ( $sql, @binds ) = $builder->update_multi($table, \@rows, +{ bar => 'updated' });
-    ( $sql, @binds ) = $builder->update_multi($table, [qw/bar john/], [ map { @$_{qw/bar john/} } @rows ], +{ bar => 'updated' });
+    ( $sql, @binds ) = $builder->insert_multi($table, \@rows, +{ update => +{ bar => 'updated' } });
+    ( $sql, @binds ) = $builder->insert_multi($table, [qw/bar john/], [ map { @$_{qw/bar john/} } @rows ], +{ update => +{ bar => 'updated' } });
 
 =head1 DESCRIPTION
 
---
+++ t/plugins/insert_multi/01_insert_multi.t
@@ -0,0 +1,147 @@
+use strict;
+use warnings;
+use Test::More;
+use SQL::Maker;
+use Test::Requires 'Tie::IxHash';
+
+sub ordered_hashref {
+    tie my %params, Tie::IxHash::, @_;
+    return \%params;
+}
+
+SQL::Maker->load_plugin('InsertMulti');
+
+subtest 'mysql' => sub {
+    subtest 'insert_multi( $table, \@colvals, \%opts )' => sub {
+        subtest 'ok' => sub {
+            my $builder = SQL::Maker->new( driver => 'mysql' );
+            my ( $sql, @binds ) = $builder->insert_multi(
+                'foo' => [
+                    ordered_hashref( bar => 'baz', john => 'man' ),
+                    ordered_hashref( bar => 'bee', john => 'row' )
+                ]
+            );
+            is $sql, "INSERT INTO `foo`\n(`bar`, `john`)\nVALUES (?, ?),\n(?, ?)";
+            is join( ',', @binds ), 'baz,man,bee,row';
+        };
+
+        subtest 'confused' => sub {
+            my $builder = SQL::Maker->new( driver => 'mysql' );
+            my ( $sql, @binds ) = $builder->insert_multi(
+                'foo' => [
+                    ordered_hashref( bar  => 'baz', john => 'man' ),
+                    ordered_hashref( john => 'row', bar  => 'bee' )
+                ]
+            );
+            is $sql, "INSERT INTO `foo`\n(`bar`, `john`)\nVALUES (?, ?),\n(?, ?)";
+            is join( ',', @binds ), 'baz,man,bee,row';
+        };
+
+        subtest 'insert ignore' => sub {
+            my $builder = SQL::Maker->new( driver => 'mysql' );
+            my ( $sql, @binds ) = $builder->insert_multi(
+                'foo' => [
+                    ordered_hashref( bar => 'baz', john => 'man' ),
+                    ordered_hashref( bar => 'bee', john => 'row' )
+                ],
+                +{ prefix => 'INSERT IGNORE' },
+            );
+            is $sql, "INSERT IGNORE `foo`\n(`bar`, `john`)\nVALUES (?, ?),\n(?, ?)";
+            is join( ',', @binds ), 'baz,man,bee,row';
+        };
+
+        subtest 'on duplicate key update' => sub {
+            my $builder = SQL::Maker->new( driver => 'mysql' );
+            my ( $sql, @binds ) = $builder->insert_multi(
+                'foo' => [
+                    ordered_hashref(
+                        bar        => 'baz',
+                        john       => 'man',
+                        created_on => \"UNIX_TIMESTAMP()",
+                        updated_on => \[ "UNIX_TIMESTAMP(?)", "2011-04-12" ]
+                    ),
+                    ordered_hashref(
+                        bar        => 'bee',
+                        john       => 'row',
+                        created_on => \"UNIX_TIMESTAMP()",
+                        updated_on => \[ "UNIX_TIMESTAMP(?)", "2011-04-13" ]
+                    ),
+                ],
+                +{
+                    update => ordered_hashref(
+                        bar        => \"VALUES(bar)",
+                        john       => "john",
+                        updated_on => \[ "UNIX_TIMESTAMP(?)", "2011-04-14" ]
+                    )
+                },
+            );
+            is $sql, substr(<< 'SQL', 0, -1);
+INSERT INTO `foo`
+(`bar`, `john`, `created_on`, `updated_on`)
+VALUES (?, ?, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(?)),
+(?, ?, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(?))
+ON DUPLICATE KEY UPDATE `bar` = VALUES(bar), `john` = ?, `updated_on` = UNIX_TIMESTAMP(?)
+SQL
+            is join( ',', @binds ), 'baz,man,2011-04-12,bee,row,2011-04-13,john,2011-04-14';
+        };
+    };
+
+    subtest 'insert_multi( $table, \@cols, \@values, \%opts )' => sub {
+        subtest 'ok' => sub {
+            my $builder = SQL::Maker->new( driver => 'mysql' );
+            my ( $sql, @binds ) = $builder->insert_multi(
+                'foo' => [qw/bar john/],
+                [ [qw/baz man/], [qw/bee row/], ],
+            );
+            is $sql, "INSERT INTO `foo`\n(`bar`, `john`)\nVALUES (?, ?),\n(?, ?)";
+            is join( ',', @binds ), 'baz,man,bee,row';
+        };
+
+        subtest 'insert ignore' => sub {
+            my $builder = SQL::Maker->new( driver => 'mysql' );
+            my ( $sql, @binds ) = $builder->insert_multi(
+                'foo' => [qw/bar john/],
+                [ [qw/baz man/], [qw/bee row/], ],
+                +{ prefix => 'INSERT IGNORE' },
+            );
+            is $sql, "INSERT IGNORE `foo`\n(`bar`, `john`)\nVALUES (?, ?),\n(?, ?)";
+            is join( ',', @binds ), 'baz,man,bee,row';
+        };
+
+        subtest 'on duplicate key update' => sub {
+            my $builder = SQL::Maker->new( driver => 'mysql' );
+            my ( $sql, @binds ) = $builder->insert_multi(
+                'foo' => [qw/bar john created_on updated_on/],
+                [
+                    [
+                        'baz', 'man', \"UNIX_TIMESTAMP()",
+                        \[ "UNIX_TIMESTAMP(?)", "2011-04-12" ]
+                    ],
+                    [
+                        'bee', 'row', \"UNIX_TIMESTAMP()",
+                        \[ "UNIX_TIMESTAMP(?)", "2011-04-13" ]
+                    ],
+                ],
+                +{
+                    update => [
+                        bar        => \"VALUES(bar)",
+                        john       => "john",
+                        updated_on => \[ "UNIX_TIMESTAMP(?)", "2011-04-14" ],
+                    ],
+                },
+            );
+            is $sql, substr( << 'SQL', 0, -1 );
+INSERT INTO `foo`
+(`bar`, `john`, `created_on`, `updated_on`)
+VALUES (?, ?, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(?)),
+(?, ?, UNIX_TIMESTAMP(), UNIX_TIMESTAMP(?))
+ON DUPLICATE KEY UPDATE `bar` = VALUES(bar), `john` = ?, `updated_on` = UNIX_TIMESTAMP(?)
+SQL
+            is join( ',', @binds ),
+              'baz,man,2011-04-12,bee,row,2011-04-13,john,2011-04-14';
+        };
+    };
+};
+
+done_testing;
+
