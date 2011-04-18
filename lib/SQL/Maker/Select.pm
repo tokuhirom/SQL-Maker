224452db94bc6936079cf6a897b388a82a4ea85b
yamaguchi.toru <zigorou@cpan.org>
2011-04-18 21:38:43 +0900

    Allow no condition join. And formatted and labeled each tests.

--- lib/SQL/Maker/Select.pm
+++ lib/SQL/Maker/Select.pm
@@ -157,11 +157,13 @@ sub as_sql {
             $sql .= ' ' . uc($join->{type}) . ' JOIN ' . $self->_quote($join->{table});
             $sql .= ' ' . $self->_quote($join->{alias}) if $join->{alias};
 
-            if (ref $join->{condition} && ref $join->{condition} eq 'ARRAY') {
-                $sql .= ' USING ('. join(', ', map { $self->_quote($_) } @{ $join->{condition} }) . ')';
-            }
-            else {
-                $sql .= ' ON ' . $join->{condition};
+            if ( defined $join->{condition} ) {
+                if (ref $join->{condition} && ref $join->{condition} eq 'ARRAY') {
+                    $sql .= ' USING ('. join(', ', map { $self->_quote($_) } @{ $join->{condition} }) . ')';
+                }
+                else {
+                    $sql .= ' ON ' . $join->{condition};
+                }
             }
         }
         $sql .= ', ' if @{ $self->{from} };
