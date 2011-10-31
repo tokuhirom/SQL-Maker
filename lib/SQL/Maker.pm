0f7ba2e2dc99481eaf7d5c28901530243a4314f3
Tokuhiro Matsuno <tokuhirom@gmail.com>
2011-11-01 06:42:13 +0900

    modernize

--- lib/SQL/Maker.pm
+++ lib/SQL/Maker.pm
@@ -480,7 +480,7 @@ So, this module contains L<SQL::Maker::Select>, the extensible B<SELECT> clause 
 
 =head1 AUTHOR
 
-Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF GMAIL COME<gt>
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
 
 =head1 SEE ALSO
 
