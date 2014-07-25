b7cd765a307559f6af7f7f0d10178b27e9098d66
Tokuhiro Matsuno <tokuhirom@gmail.com>
2014-07-26 08:56:13 +0900

    Checking in changes prior to tagging of version 1.18.
    Changelog diff is:
    
    diff --git Changes Changes
    index 3b3329e..62338c6 100644
    --- Changes
    +++ Changes
    @@ -2,6 +2,10 @@ Revision history for Perl module SQL::Maker
    
     {{$NEXT}}
    
    +1.18 2014-07-25T23:52:14Z
    +
    +    Handle `limit 0` correctly.
    +
     1.17 2014-06-16T05:24:23Z
    
         [BUG FIX]

--- lib/SQL/Maker.pm
+++ lib/SQL/Maker.pm
@@ -2,7 +2,7 @@ package SQL::Maker;
 use strict;
 use warnings;
 use 5.008001;
-our $VERSION = '1.17';
+our $VERSION = '1.18';
 use Class::Accessor::Lite 0.05 (
     ro => [qw/quote_char name_sep new_line strict driver select_class/],
 );
